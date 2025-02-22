c11732d43251cf0696a3ff684f4a10464779e033
Amir Daneshmand <amirdanesh02@gmail.com>
2025-02-23 01:26:03 +0330

    Add user service utility class.

---
+++ server/src/services/user.service.ts
@@ -0,0 +1,37 @@
+import { Model } from "mongoose";
+import { ObjectId } from "mongodb";
+
+import userModel from "../models/user.model";
+
+import { User, Users } from "../types/user.type";
+
+export default class UserService {
+  private model: Model<User>;
+
+  constructor() {
+    this.model = userModel;
+  }
+
+  public async getAllUsers(): Promise<Users> {
+    return await this.model.find();
+  }
+
+  public async getUserById(id: ObjectId): Promise<User | null> {
+    return await this.model.findById(id);
+  }
+
+  public async getUserByEmail(email: string): Promise<User | null> {
+    return await this.model.findOne({ email });
+  }
+
+  public async createNewUser(data: Record<string, User>): Promise<User> {
+    return new this.model(data);
+  }
+
+  public async updateUserById(
+    id: ObjectId,
+    data: Record<string, User>
+  ): Promise<User | null> {
+    return this.model.findByIdAndUpdate(id, data);
+  }
+}
