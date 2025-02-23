caf86dfd8b226d56cc24809f8f41222733e4023c
Amir Daneshmand <amirdanesh02@gmail.com>
2025-02-24 01:39:20 +0330

    Setup session-based strategy to use.

---
+++ server/src/setup/middlewares.ts
@@ -0,0 +1,21 @@
+import { Application } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+import session from "express-session";
+import passport from "passport";
+
+// Middlewares configurations imports.
+import corsOptions from "../config/cors";
+import sessionOptions from "../config/session";
+
+export default (app: Application): void => {
+  app.use(bodyParser.json());
+  app.use(cors(corsOptions()));
+  app.use(morgan("dev"));
+  app.use(helmet());
+  app.use(session(sessionOptions()));
+  app.use(passport.initialize());
+  app.use(passport.session());
+};
